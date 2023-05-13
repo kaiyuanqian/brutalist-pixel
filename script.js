217318b2802a06115090042c1a36d290cd91c3f6
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2023-05-13 22:37:12 +1000

    add drawing and erasing

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+var isMouseDown = false;
+document.addEventListener('mousedown', () => isMouseDown = !isMouseDown);
+document.addEventListener('mouseup', () => isMouseDown = !isMouseDown);
+
 function makeGrid() {
-    const container = document.querySelector('.container');
+    const grid = document.querySelector('.grid');
     for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        container.appendChild(row);
+        grid.appendChild(row);
         for (let j = 0; j < 16; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
@@ -24,8 +28,49 @@ function makeGrid() {
     }
 }
 
-function highlightSquare() {
+function toggleMode() {
+    const erase = document.querySelector('.erase');
+    if (erase.classList.contains('off')) {
+        erase.classList.remove('off');
+        erase.style.backgroundColor = '#add8e6'; 
+        
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => square.addEventListener('mousedown', () => {
+            square.style.backgroundColor = 'white';
+        }));
+        squares.forEach(square => square.addEventListener('mouseenter', () => {
+            if (isMouseDown) {
+                square.style.backgroundColor = 'white';
+            }
+        }));
+    } else {
+        erase.classList.add('off');
+        erase.style.backgroundColor = '#f0f0f0'
 
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => square.addEventListener('mousedown', () => {
+            square.style.backgroundColor = 'black';
+        }));
+        squares.forEach(square => square.addEventListener('mouseenter', () => {
+            if (isMouseDown) {
+                square.style.backgroundColor = 'black';
+            }
+        }));
+    }
+    
 }
 
 makeGrid();
+
+const erase = document.querySelector('.erase');
+erase.addEventListener('click', toggleMode);
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('mousedown', () => {
+    square.style.backgroundColor = 'black';
+}));
+squares.forEach(square => square.addEventListener('mouseenter', () => {
+if (isMouseDown) {
+    square.style.backgroundColor = 'black';
+}
+}));
